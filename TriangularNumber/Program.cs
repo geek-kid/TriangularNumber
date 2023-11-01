bee68894fa35516932d2e67d35a1bdddecb188ff
Arya <aryatahery@gmail.com>
2023-11-01 09:35:23 +0330

    Add project files.

---
+++ TriangularNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿int startNumber, toNumber;
+ 
+Console.WriteLine("First Number: ");
+try
+{
+    startNumber = Convert.ToInt32(Console.ReadLine());
+
+}
+catch
+{
+    Console.WriteLine("bad Input...");
+    return;
+}
+Console.WriteLine("Second Number: ");
+try
+{
+    toNumber = Convert.ToInt32(Console.ReadLine());
+
+}
+catch
+{
+    Console.WriteLine("bad Input...");
+    return;
+}
+if (startNumber > toNumber)
+{
+    Console.WriteLine("start number should be less than to number");
+}
+Console.WriteLine("--------------------------------------------");
+Calc(startNumber, toNumber);
+Console.WriteLine("--------------------------------------------");
+
+
+void Calc(int startNum, int toNum)
+{
+    Console.WriteLine((startNum * (startNum + 1))/2);
+    if (startNum == toNum) return;
+    Calc(startNum + 1, toNum);
+}
